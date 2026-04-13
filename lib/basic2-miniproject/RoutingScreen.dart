8daef0f50c76f766b232bd3b9e75d0ffa3c30707
정성규 <jsg213213@naver.com>
2026-04-13 17:15:16 +0845

    mainsreen 비롯한 폴더 삽입3

--- lib/basic2-miniproject/RoutingScreen.dart
+++ lib/basic2-miniproject/RoutingScreen.dart
@@ -2,56 +2,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import '../basic3-http/_1_dummyJson/screens/dummy_user_screen.dart';
-import '../basic3-http/_2_reqres/screen/reqres_screen.dart';
-import 'AccordionNavScreen.dart';
-import 'DetailsScreen.dart';
-import 'LoginScreen2.dart';
-import 'MainScreen.dart';
-import 'SignupScreen.dart';
-import 'SplashScreen.dart';
-import 'TabBarScreen.dart';
-import 'ViewPagerScreen.dart';
 
 class RoutingScreen extends StatelessWidget {
   const RoutingScreen({super.key});
 
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
-      routes: {
-        '/main':    (context) => const MainScreen(),
-        '/signup':  (context) => const SignupScreen(),
-        '/login':   (context) => const LoginScreen2(),
-        '/details': (context) => const DetailsScreen(),
-        // 탭 메뉴 화면 추가.
-        '/tabMenuTest': (context) => const TabBarScreen(),
-        '/viewPagerTest': (context) => const PageViewScreen(),
-        '/drawerNaviTest': (context) => const AccordionNavScreen(),
-        // API 서버 호출 테스트
-        '/reqresTest': (context) => const ReqresScreen(),
-        '/dummyTest': (context) => const DummyUserScreen(),
-        // 상세 화면 이동 라우팅 추가, /dummyDetailScreen -> 파라미터 , 유저 정보를 주입해야해서,
-        // '/dummyDetailScreen': (context) => const DummyUserDetailScreen(user: user),
-        '/newsTest': (context) => const NewsScreen(),
-        '/publicDataTest': (context) => const PublicDataScreen(),
-        '/providerTest': (context) => const CounterScreen(),
-        '/providerTestPdTest': (context) => const MyPdTestScreen(),
-        '/providerTestPdTest2': (context) => const TourScreen(),
-        '/providerTestPdTest3': (context) => const TourScreen2(),
-        '/mapBasic1': (context) => const LocationScreen(),
-        '/mapBasic2': (context) => const LocationScreen2(),
-        '/mapBasic3': (context) => const MapScreen(),
-        '/mapBasic4': (context) => const MapScreen2(),
-        '/dbTest1': (context) => const DbBasicScreen(),
-        '/dbTest2': (context) => const TodoScreen(),
-
-
-
-      },
-    );
-  }
-}
--- lib/basic4-provider/providers/CounterProvider.dart
+++ lib/basic4-provider/providers/CounterProvider.dart
@@ -1,28 +1,4 @@
 import 'package:flutter/material.dart';
 
-// ChangeNotifier를 상속받아 상태 클래스 정의
-// 이 클래스의 역할,
-// 1) 상태 관리 2) 변경시, 본인 클래스를 구독하고 있는 화면(위젯클래스)에 알려주는 역할
+
 class CounterProvider extends ChangeNotifier {
-  int _count = 0; // private 상태 변수
-
-  // getter: 외부에서 읽기 전용으로 접근
-  int get count => _count;
-
-  // 상태 변경 메서드
-  void increment() {
-    _count++;             // 상태 변경
-    // 이 부분이 매우 중요함, 방금 위에서, 상태 변경(숫자가 하나 증가), 그리고, 전달하는 메서드
-    notifyListeners();    // ← 구독 중인 위젯에게 변경 알림 (재빌드 트리거)
-  }
-
-  void decrement() {
-    if (_count > 0) _count--;
-    notifyListeners();
-  }
-
-  void reset() {
-    _count = 0;
-    notifyListeners();
-  }
-}
