d2720c97684f810c3e6d63e2cc0be7cd6ca67322
정성규 <jsg213213@naver.com>
2026-04-13 17:44:03 +0845

    mainsreen 비롯한 폴더 삽입5

--- lib/basic2-miniproject/RoutingScreen.dart
+++ lib/basic2-miniproject/RoutingScreen.dart
@@ -2,13 +2,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'SplashScreen.dart';
+
 
 class RoutingScreen extends StatelessWidget {
   const RoutingScreen({super.key});
 
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
-}
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const SplashScreen(),
+        routes: {
+        }
+    );
+    }
+    }
+
--- lib/basic2-miniproject/SplashScreen.dart
+++ lib/basic2-miniproject/SplashScreen.dart
@@ -5,8 +5,22 @@ class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // 3초 후 메인 화면으로 이동 (뒤로가기 불가)
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/main');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
     throw UnimplementedError();
   }
 }
